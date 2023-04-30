4799edcbf9b282ef8e4d4fbef44083026fded8cb
Annie624 <114262700+Annie624@users.noreply.github.com>
2023-04-30 13:05:12 -0400

    Add files via upload

--- client/src/components/Login/TeachLogin.js
+++ client/src/components/Login/TeachLogin.js
@@ -3,7 +3,58 @@ import React from 'react'
 export default function TeachLogin() {
   return (
     <div>
-        <h3>Professor Login</h3>
+
+    <div>
+        <h3>Professor Sign Up</h3>
+    </div>
+
+    <div class="form-group mx-sm-3 mb-2">
+        <form class="form-inline">
+    
+            <div class="col-md-4 mb-3">
+              <label for="validationCustom01">First name</label>
+              <input type="text" class="form-control" id="validationCustom01" placeholder="First name"></input>
+              <div class="valid-feedback">
+                Looks good!
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationCustom02">Last name</label>
+              <input type="text" class="form-control" id="validationCustom02" placeholder="Last name"></input>
+              <div class="valid-feedback">
+                Looks good!
+              </div>
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="validationCustomUsername">Username</label>
+              <div class="input-group">
+                <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend"></input>
+              </div>
+            </div>
+
+
+          <div class="col-md-4 mb-3">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+          </div>
+
+          <div class="col-md-4 mb-3">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" aria-describedby="passwordHelpBlock" placeholder="Password"></input>
+            <small id="passwordHelpBlock" class="form-text text-muted">
+              Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+            </small>
+          </div>
+          
+          <div class="col-md-4 mb-3">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1"></input>
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+
+      </div>
+
     </div>
   )
 }
